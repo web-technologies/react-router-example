2acbd293caee07158672dea03ba4e2af84af85f5
web-technologies <web-technologies@yandex.ru>
2018-05-07 00:52:27 +0300

    Second Commit
    Test Second Commit

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,15 +1,7 @@
-import React from 'react'
-
-
-
+import React from "react";
 
 // The Roster component matches one of two different routes
 // depending on the full pathname
-const MyComponent = () => (
-  <div>
-    I'm MyComponent
-  </div>
-)
+const MyComponent = () => <div>I'm MyComponent!!!</div>;
 
-
-export default MyComponent
+export default MyComponent;
